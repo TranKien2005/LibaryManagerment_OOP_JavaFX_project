eb60e50df21174d8f6450b90946c13c7882c114f
Tran_Kien <ttk08112005@gmail.com>
2024-09-27 22:58:50 +0700

    testing

---
+++ MavenTest/demo/src/main/java/test/Main.java
@@ -0,0 +1,14 @@
+package test;
+public class Main {
+    public static int Maxnumber(int a,int b) {
+        if (a >= b) {
+            return a;
+        }
+        else {
+            return b;
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ MavenTest/demo/src/test/java/test/MainTest.java
@@ -0,0 +1,10 @@
+package test;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class MainTest {
+    @Test
+    public void testMaxnumber1() {
+        assertEquals(3,Main.Maxnumber(2,3));
+    }
+}
