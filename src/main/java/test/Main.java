74d5d47c8540574022c98157684787ec305f09c2
Tran_Kien <23020087@gmail.com>
2024-10-01 16:24:28 +0700

    update

--- src/main/java/test/Main.java
+++ src/main/java/test/Main.java
@@ -9,7 +9,6 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) throws Exception {
         stage.setTitle("Kien");
-        stage.getIcons().add(new Image("file:D:/Images/Game_Image/Setup/General/Icon/0f62924d-5dac-63b1-5a83-e1d3fcd6dd27_1_11zon.png"));
         StackPane root = new StackPane();
         root.getChildren().add(new Label("kien"));
         Scene scene = new Scene(root,400,400);
