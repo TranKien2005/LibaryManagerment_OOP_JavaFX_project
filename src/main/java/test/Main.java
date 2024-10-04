ecd8a5224397f66d15e259aac4e54712e41378ef
Tran_Kien <23020087@gmail.com>
2024-10-04 15:42:45 +0700

    update book

--- src/main/java/test/Main.java
+++ src/main/java/test/Main.java
@@ -1,6 +1,7 @@
 package test;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 import javafx.application.Application;
 import javafx.scene.control.*;
@@ -8,27 +9,36 @@ import javafx.scene.image.*;
 import javafx.scene.layout.*;
 import javafx.scene.*;
 import javafx.stage.*;
+
 public class Main extends Application {
-    @Override
+    private static final String URL = "jdbc:mysql://localhost:3306/librarymanagement";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
     public void start(Stage stage) throws Exception {
         stage.setTitle("Kien");
         StackPane root = new StackPane();
         root.getChildren().add(new Label("kien"));
-        Scene scene = new Scene(root,400,400);
+        Scene scene = new Scene(root, 400, 400);
         stage.setScene(scene);
         stage.show();
     }
+
     public static void main(String[] args) {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection connection = DriverManager.getConnection(
-            "jdbc:mysql://localhost:3306/classicmodels", "root", "");
+            Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
             System.out.println("Connected to the database!");
-            connection.close();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        // launch(args);
+       Book book1 = new Book("kien","kien","jjkk",2);
+       try {
+           book1.addBookToDatabase(URL, USER, PASSWORD);
+           Book.printAllBooks(URL, USER, PASSWORD);
+       } catch (SQLException e) {
+           e.printStackTrace();
+       }
+        launch(args);
     }
-    }
-    
+}
